ae107abe3506cf417ec50b9ab74de04cf7da3a79
El Mehdi Rami <lbiggboss19@gmail.com>
2021-04-25 04:34:52 +0000

    adding some docstrings to document the methods

--- flask_mux/mux.py
+++ flask_mux/mux.py
@@ -8,8 +8,21 @@ class Mux:
         self.rules = []
 
     def use(self, namespace: str, router: Router):
+        """Registers all the router's routes with their endpoints
+        in the provided namespace.
+
+        Example:
+
+            use('/auth', auth_router) will take all the routes created
+            using the auth_router, prepend '/auth' to each route's endpoint
+            and then register the route with the final endpoint.
+
+        Args:
+            namespace (str): namespace which the routes will be mapped to.
+            router (Router): router instance with the registered routes.
+        """
         for route in router.routes:
-            rule = self.create_rule(namespace, route)
+            rule = self._create_rule(namespace, route)
 
             if rule.view_func:
                 self.app.add_url_rule(
@@ -17,11 +30,27 @@ class Mux:
 
                 self.rules.append(rule)
 
-    def create_rule(self, namespace: str, route: Route):
+    def _create_rule(self, namespace: str, route: Route) -> Rule:
+        """Creates a Rule instance with the correct url rule and endpoint.
+
+        Args:
+            namespace (str): namespace which the route param will be mapped to.
+            route (Route): route instance that will be registered with the provided namespace.
+        """
+
         _namespace = namespace.strip('/')
         _endpoint = route.endpoint.strip('/')
+
+        # if namespace = '/' -> no modifications are needed for url rule and the endpoint
         if _namespace == '':
             return Rule(route.endpoint, route.view_func.__name__, route.view_func)
+
+        """example for namespace != '/':
+        namespace='/auth'       Route.endpoint='/login'     view_func.__name__='login_func'
+        
+        --> Rule.rule = '/auth/login'
+        --> Rule.endpoint = 'auth.login_func'
+        """
 
         url_rule = f'/{_namespace}/{_endpoint}'
         endpoint = f'{_namespace}.{route.view_func.__name__}'
--- flask_mux/router.py
+++ flask_mux/router.py
@@ -7,6 +7,14 @@ class Router:
         self.routes: List[Route] = []
 
     def route(self, endpoint: str, http_methods: list = None):
+        """Acts similarly to Flask.route decorator.
+        Appends a new Route instance to the self.routes list
+        which will be used to register all the routes with their endpoints.
+
+        Args:
+            endpoint (str): endpoint to handle.
+            http_methods (list): allowed HTTP methods for the given endpoint.
+        """
         def decorator(view_func):
             route = Route(endpoint, view_func, http_methods=http_methods)
             self.routes.append(route)
@@ -18,9 +26,46 @@ class Router:
             return wrapper
         return decorator
 
+    def get(self, endpoint: str, *middlewares):
+        """Handles incoming HTTP GET requests 
+        by executing the passed middlewares in
+        their respective order.
+
+        Args:
+            endpoint (str): endpoint to handle.
+            middlewares (*Callable): variadic param representing a sequence 
+            of middlewares.
+        """
+        if not middlewares:
+            raise Exception('route handler missing')
+
+        middlewares = list(middlewares)
+        for mw in middlewares:
+            if not isinstance(mw, Callable):
+                raise TypeError(f'{mw} is not callable')
+
+        view_func = middlewares.pop()
+        if not middlewares:
+            route = Route(endpoint, view_func, http_methods=['GET'])
+            self.routes.append(route)
+            return
+
+        mw = middlewares.pop(0)
+
+        while middlewares:
+            mw = mw(middlewares.pop(0))
+
+        view_func = mw(view_func)
+        route = Route(endpoint, view_func, http_methods=['GET'])
+        self.routes.append(route)
+        return
+
 
 class Route:
     def __init__(self, endpoint: str, view_func: Callable, http_methods: list = None):
         self.endpoint: str = endpoint
         self.view_func = view_func
         self.http_methods = http_methods or ['GET']
+
+    def __repr__(self):
+        return f'{self.endpoint} -> {self.http_methods}'
