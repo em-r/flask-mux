5d9ac10e1c062537ef7ea489c29b65b7434fcda4
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-02 00:22:48 +0000

    middlewares approach POST method

--- tests/test_mws.py
+++ tests/test_mws.py
@@ -4,7 +4,7 @@ from flask_mux import Mux
 from tests.test_mws_cases import test_mws_router
 
 
-class TestMwsGet(unittest.TestCase):
+class TestMwsGET(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
         self.mux = Mux(self.app)
@@ -24,6 +24,7 @@ class TestMwsGet(unittest.TestCase):
         self.assertTrue(resp.json.get('success'))
         self.assertEqual(resp.json.get('admin'), headers.get('admin'))
 
+    def test_one_mw_failing(self):
         resp = self.client.get('/get-with-auth')
         self.assertEqual(resp.status_code, 403)
         self.assertFalse(resp.json.get('success'))
@@ -38,6 +39,7 @@ class TestMwsGet(unittest.TestCase):
         self.assertTrue(resp.json.get('success'))
         self.assertEqual(resp.json.get('admin'), headers.get('admin'))
 
+    def test_multi_mws_failing(self):
         resp = self.client.get('/get-with-auth')
         self.assertEqual(resp.status_code, 403)
         self.assertFalse(resp.json.get('success'))
@@ -51,6 +53,84 @@ class TestMwsGet(unittest.TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(resp.json.get('success'))
         self.assertEqual(resp.json.get('admin'), headers.get('admin'))
+
+
+class TestMwsPOST(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.mux = Mux(self.app)
+        self.mux.use('/', test_mws_router)
+
+        self.client = self.app.test_client()
+
+    def test_basic(self):
+        resp = self.client.post('/basic')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_one_mw(self):
+        body = {'message': 'samcro'}
+        resp = self.client.post('/one-mw', json=body)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.json.get('success'))
+        self.assertDictEqual(resp.json.get('req_body'), body)
+
+    def test_one_mw_failing(self):
+        resp = self.client.post('/one-mw')
+        self.assertEqual(resp.status_code, 400)
+        self.assertFalse(resp.json.get('success'))
+
+    def test_multi_mws(self):
+        body = {'message': 'samcro'}
+        headers = {'Authorization': 'whatever'}
+        resp = self.client.post('/multi-mws', json=body, headers=headers)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.json.get('success'))
+        self.assertDictEqual(resp.json.get('req_body'), body)
+
+    def test_multi_mws_failing(self):
+        body = {'message': 'samcro'}
+        resp = self.client.post('/multi-mws', json=body)
+
+        self.assertEqual(resp.status_code, 401)
+        self.assertFalse(resp.json.get('success'))
+        self.assertEqual(resp.json.get('message'), 'unauthorized access')
+
+    def test_extra_mws(self):
+        body = {'message': 'samcro'}
+        headers = {'Authorization': 'whatever', 'admin': 'Mehdi'}
+        resp = self.client.post('/extra-mws', json=body, headers=headers)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.json.get('success'))
+        self.assertEqual(resp.json.get('admin'), headers.get('admin'))
+        self.assertDictEqual(resp.json.get('req_body'), body)
+
+    def test_extra_mws_failing_1(self):
+        body = {'message': 'samcro'}
+        headers = {'admin': 'Mehdi'}
+        resp = self.client.post('/extra-mws', json=body, headers=headers)
+
+        self.assertEqual(resp.status_code, 401)
+        self.assertFalse(resp.json.get('success'))
+        self.assertEqual(resp.json.get('message'), 'unauthorized access')
+
+    def test_extra_mws_failing_2(self):
+        body = {'message': 'samcro'}
+        headers = {'Authorization': 'whatever'}
+        resp = self.client.post('/extra-mws', json=body, headers=headers)
+
+        self.assertEqual(resp.status_code, 403)
+        self.assertFalse(resp.json.get('success'))
+        self.assertEqual(resp.json.get('message'), 'only admins are allowed')
+
+    def test_extra_mws_failing_3(self):
+        headers = {'Authorization': 'whatever', 'admin': 'Mehdi'}
+        resp = self.client.post('/extra-mws', headers=headers)
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertFalse(resp.json.get('success'))
 
 
 if __name__ == '__main__':
--- tests/test_mws_cases.py
+++ tests/test_mws_cases.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_mux import Router
-from tests.common import is_auth, is_admin, mock_middleware
+from tests.common import is_auth, is_admin, is_json, mock_middleware
+
+test_mws_router = Router()
 
 
 def get_basic():
@@ -19,7 +21,6 @@ def get_with_extra_mws():
     return {'success': True, 'admin': request.headers.get('admin')}
 
 
-test_mws_router = Router()
 test_mws_router.get('/basic', get_basic)
 test_mws_router.get('/get-with-auth', is_admin, get_with_auth)
 test_mws_router.get('/get-with-multi-mws', is_auth,
@@ -32,3 +33,26 @@ test_mws_router.get('/get-with-extra-mws',
                     is_admin,
                     get_with_extra_mws
                     )
+
+
+def post_basic():
+    return {'success': True}
+
+
+def post_with_one_mw():
+    return {'success': True, 'req_body': request.json}
+
+
+def post_with_multi_mws():
+    return {'success': True, 'req_body': request.json}
+
+
+def post_with_extra_mws():
+    return {'success': True, 'req_body': request.json, 'admin': request.headers.get('admin')}
+
+
+test_mws_router.post('/basic', post_basic)
+test_mws_router.post('/one-mw', is_json, post_with_one_mw)
+test_mws_router.post('/multi-mws', is_auth, is_json, post_with_multi_mws)
+test_mws_router.post('/extra-mws', mock_middleware, mock_middleware, mock_middleware,
+                     mock_middleware, is_auth, is_admin, is_json, post_with_extra_mws)
