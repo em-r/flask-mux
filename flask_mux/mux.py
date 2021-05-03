5a42e3e0c3732b7b18536e08a4f1d5048568e2d4
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-03 22:27:54 +0000

    moved create_rule method to the Rule class as a classmethod

--- flask_mux/mux.py
+++ flask_mux/mux.py
@@ -3,6 +3,26 @@ from flask_mux.router import Router, Route
 
 
 class Rule:
+    """Object representation of a URL rule.
+
+    The Rule class is used to represent url rules upon 
+    when they are being registered using the 'Flask.add_url_rule' 
+    when calling the Mux.use method on a router.
+
+
+    Properties:
+        rule (str): url rule to be registered.
+        endpoint (str): endpoint for the registered rule.
+        view_func (callable): function to be called when a request
+            hits the endpoint.
+
+
+    Methods:
+        create_from_route(namespace, route):
+            Creates a new Rule instance based on the provided
+            namespace and Route. 
+    """
+
     def __init__(self, rule: str = '', endpoint: str = '', view_func: callable = None):
         self.rule = rule
         self.endpoint = endpoint
@@ -10,6 +30,39 @@ class Rule:
 
     def __repr__(self):
         return f'rule: {self.rule} | endpoint: {self.endpoint}'
+
+    @classmethod
+    def create_from_route(cls, namespace: str, route: Route):
+        """Creates a Rule instance using the provided route properties.
+
+        Args:
+            namespace (str): namespace which the route parameter
+                will be mapped to.
+            route (Route): route instance that will be registered 
+                with the provided namespace.
+
+        Returns:
+            Rule: new Rule instance linked to the provided namespace.
+        """
+
+        _namespace = namespace.strip('/')
+        _endpoint = route.endpoint.strip('/')
+
+        # if namespace = '/' -> no modifications are needed for url rule and the endpoint
+        if _namespace == '':
+            return Rule(route.endpoint, route.unwrapped_view_func.__name__, route.view_func)
+
+        """example for namespace != '/':
+        namespace='/auth'       Route.endpoint='/login'     view_func.__name__='login_func'
+        
+        --> Rule.rule = '/auth/login'
+        --> Rule.endpoint = 'auth.login_func'
+        """
+
+        url_rule = f'/{_namespace}/{_endpoint}'
+        _namespace = _namespace.replace('/', '.')
+        endpoint_path = f'{_namespace}.{route.unwrapped_view_func.__name__}'
+        return Rule(url_rule, endpoint_path, route.view_func)
 
 
 class Mux:
@@ -32,37 +85,11 @@ class Mux:
             router (Router): router instance with the registered routes.
         """
         for route in router.routes:
-            rule = self._create_rule(namespace, route)
+            # rule = self._create_rule(namespace, route)
+            rule = Rule.create_from_route(namespace, route)
 
             if rule.view_func:
                 self.app.add_url_rule(
                     rule.rule, rule.endpoint, rule.view_func, methods=route.http_methods)
 
                 self.rules.append(rule)
-
-    def _create_rule(self, namespace: str, route: Route) -> Rule:
-        """Creates a Rule instance with the correct url rule and endpoint.
-
-        Args:
-            namespace (str): namespace which the route param will be mapped to.
-            route (Route): route instance that will be registered with the provided namespace.
-        """
-
-        _namespace = namespace.strip('/')
-        _endpoint = route.endpoint.strip('/')
-
-        # if namespace = '/' -> no modifications are needed for url rule and the endpoint
-        if _namespace == '':
-            return Rule(route.endpoint, route.unwrapped_view_func.__name__, route.view_func)
-
-        """example for namespace != '/':
-        namespace='/auth'       Route.endpoint='/login'     view_func.__name__='login_func'
-        
-        --> Rule.rule = '/auth/login'
-        --> Rule.endpoint = 'auth.login_func'
-        """
-
-        url_rule = f'/{_namespace}/{_endpoint}'
-        _namespace = _namespace.replace('/', '.')
-        endpoint_path = f'{_namespace}.{route.unwrapped_view_func.__name__}'
-        return Rule(url_rule, endpoint_path, route.view_func)
