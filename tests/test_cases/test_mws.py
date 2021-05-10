6d49fd43b7a25f16efd00109d94045c150137c79
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-10 23:39:26 +0000

    added unittest for Router.delete and Router.patch methods

--- tests/test_cases/test_mws.py
+++ tests/test_cases/test_mws.py
@@ -6,96 +6,212 @@ test_mws_router = Router()
 
 
 def get_basic():
-    return {'success': True}
+    return {"success": True}
 
 
 def get_with_auth():
-    return {'success': True, 'admin': request.headers.get('admin')}
+    return {"success": True, "admin": request.headers.get("admin")}
 
 
 def get_with_multi_mws():
-    return {'success': True, 'admin': request.headers.get('admin')}
+    return {"success": True, "admin": request.headers.get("admin")}
 
 
 def get_with_extra_mws():
-    return {'success': True, 'admin': request.headers.get('admin')}
+    return {"success": True, "admin": request.headers.get("admin")}
 
 
-test_mws_router.get('/basic', get_basic)
-test_mws_router.get('/get-with-auth', is_admin, get_with_auth)
-test_mws_router.get('/get-with-multi-mws', is_auth,
-                    is_admin, get_with_multi_mws)
-test_mws_router.get('/get-with-extra-mws',
-                    mock_middleware,
-                    mock_middleware,
-                    mock_middleware,
-                    is_auth,
-                    is_admin,
-                    get_with_extra_mws
-                    )
+test_mws_router.get("/basic", get_basic)
+test_mws_router.get("/get-with-auth", is_admin, get_with_auth)
+test_mws_router.get("/get-with-multi-mws", is_auth, is_admin, get_with_multi_mws)
+test_mws_router.get(
+    "/get-with-extra-mws",
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    is_auth,
+    is_admin,
+    get_with_extra_mws,
+)
 
 
 def post_basic():
-    return {'success': True}
+    return {"success": True}
 
 
 def post_with_one_mw():
-    return {'success': True, 'req_body': request.json}
+    return {"success": True, "req_body": request.json}
 
 
 def post_with_multi_mws():
-    return {'success': True, 'req_body': request.json}
+    return {"success": True, "req_body": request.json}
 
 
 def post_with_extra_mws():
-    return {'success': True, 'req_body': request.json, 'admin': request.headers.get('admin')}
+    return {
+        "success": True,
+        "req_body": request.json,
+        "admin": request.headers.get("admin"),
+    }
 
 
-test_mws_router.post('/basic', post_basic)
-test_mws_router.post('/one-mw', is_json, post_with_one_mw)
-test_mws_router.post('/multi-mws', is_auth, is_json, post_with_multi_mws)
-test_mws_router.post('/extra-mws', mock_middleware, mock_middleware, mock_middleware,
-                     mock_middleware, is_auth, is_admin, is_json, post_with_extra_mws)
+test_mws_router.post("/basic", post_basic)
+test_mws_router.post("/one-mw", is_json, post_with_one_mw)
+test_mws_router.post("/multi-mws", is_auth, is_json, post_with_multi_mws)
+test_mws_router.post(
+    "/extra-mws",
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    is_auth,
+    is_admin,
+    is_json,
+    post_with_extra_mws,
+)
 
 
 def handle_basic():
-    return {'success': True, 'method': request.method}
+    return {"success": True, "method": request.method}
 
 
 def handle_with_one_mw():
-    return {'success': True, 'method': request.method}
+    return {"success": True, "method": request.method}
 
 
 def handle_with_multi_mws():
-    return {
-        'success': True,
-        'req_body': request.json,
-        'method': request.method
-    }
+    return {"success": True, "req_body": request.json, "method": request.method}
 
 
 def handle_with_extra_mws():
     return {
-        'success': True,
-        'req_body': request.json,
-        'admin': request.headers.get('admin'),
-        'method': request.method
+        "success": True,
+        "req_body": request.json,
+        "admin": request.headers.get("admin"),
+        "method": request.method,
     }
 
 
-test_mws_router.handle('/handle-basic', handle_basic)
-test_mws_router.handle('/handle-one-mw', is_auth, handle_with_one_mw)
-test_mws_router.handle('/handle-multi-mws',
-                       is_auth,
-                       is_json,
-                       handle_with_multi_mws)
+test_mws_router.handle("/handle-basic", handle_basic)
+test_mws_router.handle("/handle-one-mw", is_auth, handle_with_one_mw)
+test_mws_router.handle("/handle-multi-mws", is_auth, is_json, handle_with_multi_mws)
 
-test_mws_router.handle('/handle-extra-mws',
-                       mock_middleware,
-                       mock_middleware,
-                       mock_middleware,
-                       mock_middleware,
-                       is_auth,
-                       is_admin,
-                       is_json,
-                       handle_with_extra_mws)
+test_mws_router.handle(
+    "/handle-extra-mws",
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    is_auth,
+    is_admin,
+    is_json,
+    handle_with_extra_mws,
+)
+
+
+def put_basic():
+    return {"success": True}
+
+
+def put_with_one_mw():
+    return {"success": True, "req_body": request.json}
+
+
+def put_with_multi_mws():
+    return {"success": True, "req_body": request.json}
+
+
+def put_with_extra_mws():
+    return {
+        "success": True,
+        "req_body": request.json,
+        "admin": request.headers.get("admin"),
+    }
+
+
+test_mws_router.put("/basic", put_basic)
+test_mws_router.put("/one-mw", is_json, put_with_one_mw)
+test_mws_router.put("/multi-mws", is_auth, is_json, put_with_multi_mws)
+test_mws_router.put(
+    "/extra-mws",
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    is_auth,
+    is_admin,
+    is_json,
+    put_with_extra_mws,
+)
+
+
+def delete_basic():
+    return {"success": True}
+
+
+def delete_with_one_mw():
+    return {"success": True, "req_body": request.json}
+
+
+def delete_with_multi_mws():
+    return {"success": True, "req_body": request.json}
+
+
+def delete_with_extra_mws():
+    return {
+        "success": True,
+        "req_body": request.json,
+        "admin": request.headers.get("admin"),
+    }
+
+
+test_mws_router.delete("/basic", delete_basic)
+test_mws_router.delete("/one-mw", is_json, delete_with_one_mw)
+test_mws_router.delete("/multi-mws", is_auth, is_json, delete_with_multi_mws)
+test_mws_router.delete(
+    "/extra-mws",
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    is_auth,
+    is_admin,
+    is_json,
+    delete_with_extra_mws,
+)
+
+
+def patch_basic():
+    return {"success": True}
+
+
+def patch_with_one_mw():
+    return {"success": True, "req_body": request.json}
+
+
+def patch_with_multi_mws():
+    return {"success": True, "req_body": request.json}
+
+
+def patch_with_extra_mws():
+    return {
+        "success": True,
+        "req_body": request.json,
+        "admin": request.headers.get("admin"),
+    }
+
+
+test_mws_router.patch("/basic", patch_basic)
+test_mws_router.patch("/one-mw", is_json, patch_with_one_mw)
+test_mws_router.patch("/multi-mws", is_auth, is_json, patch_with_multi_mws)
+test_mws_router.patch(
+    "/extra-mws",
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    mock_middleware,
+    is_auth,
+    is_admin,
+    is_json,
+    patch_with_extra_mws,
+)
--- tests/test_put.py
+++ tests/test_put.py
@@ -3,19 +3,19 @@ from tests.test_base import FlaskMuxBaseTest
 from tests.test_cases.test_mws import test_mws_router
 
 
-class TestMwsPOST(FlaskMuxBaseTest):
+class TestMwsPUT(FlaskMuxBaseTest):
     def setUp(self):
         super().setUp()
         self.mux.use('/', test_mws_router)
-        self.req_func = self.client.post
+        self.req_func = self.client.put
 
     def test_basic(self):
-        """test POST method with no middlewares"""
+        """test PUT method with no middlewares"""
         resp = self.req_func('/basic')
         self.assertEqual(resp.status_code, 200)
 
     def test_one_mw(self):
-        """test POST method with one middleware"""
+        """test PUT method with one middleware"""
         body = {'message': 'samcro'}
         resp = self.req_func('/one-mw', json=body)
 
@@ -24,13 +24,13 @@ class TestMwsPOST(FlaskMuxBaseTest):
         self.assertDictEqual(resp.json.get('req_body'), body)
 
     def test_one_mw_failing(self):
-        """test POST method with one middleware"""
+        """test PUT method with one middleware"""
         resp = self.req_func('/one-mw')
         self.assertEqual(resp.status_code, 400)
         self.assertFalse(resp.json.get('success'))
 
     def test_multi_mws(self):
-        """test POST method with multiple middleware"""
+        """test PUT method with multiple middleware"""
         body = {'message': 'samcro'}
         headers = {'Authorization': 'whatever'}
         resp = self.req_func('/multi-mws', json=body, headers=headers)
@@ -40,7 +40,7 @@ class TestMwsPOST(FlaskMuxBaseTest):
         self.assertDictEqual(resp.json.get('req_body'), body)
 
     def test_multi_mws_failing(self):
-        """test POST method with multiple middleware"""
+        """test PUT method with multiple middleware"""
         body = {'message': 'samcro'}
         resp = self.req_func('/multi-mws', json=body)
 
@@ -49,7 +49,7 @@ class TestMwsPOST(FlaskMuxBaseTest):
         self.assertEqual(resp.json.get('message'), 'unauthorized access')
 
     def test_extra_mws(self):
-        """test POST method with many middleware"""
+        """test PUT method with many middleware"""
         body = {'message': 'samcro'}
         headers = {'Authorization': 'whatever', 'admin': 'Mehdi'}
         resp = self.req_func('/extra-mws', json=body, headers=headers)
@@ -60,7 +60,7 @@ class TestMwsPOST(FlaskMuxBaseTest):
         self.assertDictEqual(resp.json.get('req_body'), body)
 
     def test_extra_mws_failing_1(self):
-        """test POST method with many middleware"""
+        """test PUT method with many middleware"""
         body = {'message': 'samcro'}
         headers = {'admin': 'Mehdi'}
         resp = self.req_func('/extra-mws', json=body, headers=headers)
@@ -70,7 +70,7 @@ class TestMwsPOST(FlaskMuxBaseTest):
         self.assertEqual(resp.json.get('message'), 'unauthorized access')
 
     def test_extra_mws_failing_2(self):
-        """test POST method with many middleware"""
+        """test PUT method with many middleware"""
         body = {'message': 'samcro'}
         headers = {'Authorization': 'whatever'}
         resp = self.req_func('/extra-mws', json=body, headers=headers)
@@ -80,7 +80,7 @@ class TestMwsPOST(FlaskMuxBaseTest):
         self.assertEqual(resp.json.get('message'), 'only admins are allowed')
 
     def test_extra_mws_failing_3(self):
-        """test POST method with many middleware"""
+        """test PUT method with many middleware"""
         headers = {'Authorization': 'whatever', 'admin': 'Mehdi'}
         resp = self.req_func('/extra-mws', headers=headers)
 
