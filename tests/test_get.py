362d43e0028b47af7ef1aa27922392a9d47de322
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-13 15:24:11 +0000

    rewrote Router.get tests with pytest

--- tests/test_get.py
+++ tests/test_get.py
@@ -1,61 +1,67 @@
-import unittest
-from tests.test_base import FlaskMuxBaseTest
+import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+from flask_mux import Mux
 from tests.test_cases.test_mws import test_mws_router
 
 
-class TestMwsGET(FlaskMuxBaseTest):
-    def setUp(self):
-        super().setUp()
-        self.mux.use('/', test_mws_router)
-
-    def test_basic(self):
-        """test GET method with no middlewares"""
-        resp = self.client.get('/basic')
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue(resp.json.get('success'))
-
-    def test_one_mw(self):
-        """test GET method with one middlewares"""
-        headers = {'admin': 'Mehdi'}
-        resp = self.client.get('/get-with-auth', headers=headers)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue(resp.json.get('success'))
-        self.assertEqual(resp.json.get('admin'), headers.get('admin'))
-
-    def test_one_mw_failing(self):
-        """test GET method with one middlewares"""
-        resp = self.client.get('/get-with-auth')
-        self.assertEqual(resp.status_code, 403)
-        self.assertFalse(resp.json.get('success'))
-
-    def test_multi_mws(self):
-        """test GET method with multiple middlewares"""
-        headers = {
-            'Authorization': 'whatever',
-            'admin': 'Mehdi'
-        }
-        resp = self.client.get('/get-with-multi-mws', headers=headers)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue(resp.json.get('success'))
-        self.assertEqual(resp.json.get('admin'), headers.get('admin'))
-
-    def test_multi_mws_failing(self):
-        """test GET method with multiple middlewares"""
-        resp = self.client.get('/get-with-auth')
-        self.assertEqual(resp.status_code, 403)
-        self.assertFalse(resp.json.get('success'))
-
-    def test_extra_mws(self):
-        """test GET method with many middlewares"""
-        headers = {
-            'Authorization': 'whatever',
-            'admin': 'Mehdi'
-        }
-        resp = self.client.get('/get-with-extra-mws', headers=headers)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue(resp.json.get('success'))
-        self.assertEqual(resp.json.get('admin'), headers.get('admin'))
+@pytest.fixture
+def client():
+    app = Flask(__name__)
+    mux = Mux(app)
+    mux.use('/', test_mws_router)
+    return app.test_client()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_basic(client: FlaskClient):
+    """test GET method with no middlewares"""
+    resp = client.get('/basic')
+    assert resp.status_code == 200
+    assert resp.json.get('success')
+
+
+def test_one_mw(client: FlaskClient):
+    """test GET method with one middlewares"""
+    headers = {'admin': 'Mehdi'}
+    resp = client.get('/get-with-auth', headers=headers)
+    assert resp.status_code == 200
+    assert resp.json.get('success')
+    assert resp.json.get('admin') == headers.get('admin')
+
+
+def test_one_mw_failing(client: FlaskClient):
+    """test GET method with one middlewares"""
+    resp = client.get('/get-with-auth')
+    assert resp.status_code == 403
+    assert not resp.json.get('success')
+
+
+def test_multi_mws(client: FlaskClient):
+    """test GET method with multiple middlewares"""
+    headers = {
+        'Authorization': 'whatever',
+        'admin': 'Mehdi'
+    }
+    resp = client.get('/get-with-multi-mws', headers=headers)
+    assert resp.status_code == 200
+    assert resp.json.get('success')
+    assert resp.json.get('admin') == headers.get('admin')
+
+
+def test_multi_mws_failing(client: FlaskClient):
+    """test GET method with multiple middlewares"""
+    resp = client.get('/get-with-auth')
+    assert resp.status_code == 403
+    assert not resp.json.get('success')
+
+
+def test_extra_mws(client: FlaskClient):
+    """test GET method with many middlewares"""
+    headers = {
+        'Authorization': 'whatever',
+        'admin': 'Mehdi'
+    }
+    resp = client.get('/get-with-extra-mws', headers=headers)
+    assert resp.status_code == 200
+    assert resp.json.get('success')
+    assert resp.json.get('admin') == headers.get('admin')
